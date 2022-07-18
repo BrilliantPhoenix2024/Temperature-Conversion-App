53e1b8cb101f326c199c63fa8d39d06eb548de9f
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-07-18 21:58:02 +0330

    Modify the program to Create better UI

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ lbl_farenhite = tk.Label(
 )
 
 ent_farenhite = tk.Entry(
-    master=window
+    master=window,
+    width=50,
 )
 
 btn_calc = tk.Button(
@@ -16,9 +17,9 @@ btn_calc = tk.Button(
     text='Calc',
 )
 
-lbl_farenhite.grid(row=0, column=0)
+lbl_farenhite.grid(row=0, column=0, padx=10, pady=10)
 ent_farenhite.grid(row=0, column=1)
-btn_calc.grid(row=0, column=2)
+btn_calc.grid(row=0, column=2, padx=10, pady=10)
 
 lbl_celsius = tk.Label(
     master=window,
@@ -30,7 +31,8 @@ lbl_result = tk.Label(
     text='Enter your number...'
 )
 
-lbl_celsius.grid(row=1, column=0)
-lbl_result.grid(row=1, column=1)
+lbl_celsius.grid(row=1, column=0, pady=(10, 20))
+lbl_result.grid(row=1, column=1, pady=(10, 20))
 
+window.title("Atefeh's first app")
 window.mainloop()
