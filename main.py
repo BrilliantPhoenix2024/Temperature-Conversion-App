a20535c0773d7ff4a31aa9678d74ee2c7b66b2f5
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-07-18 22:36:30 +0330

    Create 'def convert_farenhit_to_celsius()' to Emplementation the Conversion

--- main.py
+++ main.py
@@ -2,6 +2,26 @@ import tkinter as tk
 
 window = tk.Tk()
 
+farenhite_val = tk.StringVar()
+
+lbl_result = tk.Label(
+    master=window,
+    text='Enter your number...'
+)
+
+
+def convert_farenhit_to_celsius():
+    farenhit_input = farenhite_val.get()
+    try:                           # Convert to celsius
+        farenhite_value = float(farenhit_input)
+        lbl_result['text'] = (farenhite_value-32)*5/9
+    except ValueError:
+        if farenhit_input != '':    # if user input is not Valid
+            lbl_result['text'] = 'Invalid input.Please enter a Number.'
+        else:                      # if user input is Empty
+            lbl_result['text'] = 'Your input is Empty.Please enter a Number.' 
+              
+              
 lbl_farenhite = tk.Label(
     master=window,
     text='Farenhite:',
@@ -10,11 +30,13 @@ lbl_farenhite = tk.Label(
 ent_farenhite = tk.Entry(
     master=window,
     width=50,
+    textvariable=farenhite_val,
 )
 
 btn_calc = tk.Button(
     master=window,
     text='Calc',
+    command=convert_farenhit_to_celsius,
 )
 
 lbl_farenhite.grid(row=0, column=0, padx=10, pady=10)
@@ -26,10 +48,6 @@ lbl_celsius = tk.Label(
     text='Celsius:',
 )
 
-lbl_result = tk.Label(
-    master=window,
-    text='Enter your number...'
-)
 
 lbl_celsius.grid(row=1, column=0, pady=(10, 20))
 lbl_result.grid(row=1, column=1, pady=(10, 20))
